e43a76478151f9abb2131900d5b8536c9070c490
Ondrej Dockal <odockal@redhat.com>
2023-10-04 20:43:10 +0200

    add options to initialize or start machine and rootful flag
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- lib/windows/run.ps1
+++ lib/windows/run.ps1
@@ -1,9 +1,28 @@
 param(
+    [Parameter(HelpMessage='folder on target host where assets are copied')]
+    $targetFolder,
+    [Parameter(HelpMessage='Results folder')]
+    $resultsFolder="results",
     [Parameter(HelpMessage = 'Podman Download URL')]
-    [string]$downloadUrl = "https://api.cirrus-ci.com/v1/artifact/github/containers/podman/Artifacts/binary/podman-remote-release-windows_amd64.zip",
-    [Parameter(HelpMessage = 'Podman version')]
-    [string]$version = "4.8.0-dev"
+    $downloadUrl='https://api.cirrus-ci.com/v1/artifact/github/containers/podman/Artifacts/binary/podman-remote-release-windows_amd64.zip',
+    [Parameter(HelpMessage='Podman version')]
+    $version='4.8.0-dev',
+    [Parameter(HelpMessage = 'Initialize podman machine, default is 0/false')]
+    $initialize='0',
+    [Parameter(HelpMessage = 'Start Podman machine, default is 0/false')]
+    $start='0',
+    [Parameter(HelpMessage = 'Podman machine rootful flag, default 0/false')]
+    $rootful='0'
 )
+
+write-host "Print out script parameters, usefull for debugging..."
+$ParametersList = (Get-Command -Name $MyInvocation.InvocationName).Parameters;
+foreach ($key in $ParameterList.keys) {
+    $variable = Get-Variable -Name $key -ErrorAction SilentlyContinue;
+    if($variable) {
+        write-host "$($variable.name) > $($variable.value)"
+    }
+}
 
 # Function to check if a command is available
 function Command-Exists($command) {
@@ -11,18 +30,14 @@ function Command-Exists($command) {
     return $?
 }
 
-Write-Host "Podman desktop E2E podman script is being run..."
+Write-Host "Podman desktop E2E - podman nightly install script is being run..."
 
-# Specify the user profile directory
-$userProfile = $env:USERPROFILE
-
-# Specify the shared tools directory
-$toolsInstallDir = Join-Path $userProfile 'tools'
-
-# Create the tools directory if it doesn't exist
-if (-not (Test-Path -Path $toolsInstallDir -PathType Container)) {
-    New-Item -Path $toolsInstallDir -ItemType Directory
-}
+write-host "Switching to a target folder: " $targetFolder
+cd $targetFolder
+write-host "Create a resultsFolder in targetFolder: $resultsFolder"
+mkdir $resultsFolder
+$workingDir=Get-Location
+write-host "Working location: " $workingDir
 
 # Force install of WSL
 wsl -l -v
@@ -34,7 +49,7 @@ if (!$installed) {
     wsl --set-default-version 2
     $distroMissing=$?
     if($distroMissing) {
-        write-host "Wsl enabled, but distro missing"
+        write-host "Wsl enabled, but distro is missing, installing default distro..."
         wsl --install --no-launch
     }
 }
@@ -51,8 +66,29 @@ if (-not (Command-Exists "podman")) {
 }
 
 # Setup podman machine in the host system
-podman machine init --rootful true
-podman machine start
-podman machine ls
+if ($initialize -eq "1") {
+    $flags=''
+    if ($rootful) {
+        $flags="--rootful"
+    }
+    write-host "Initializing podman machine, command: podman machine init $flags"
+    $logFile = "$workingDir\$resultsFolder\podman-machine-init.log"
+    "podman machine init $flags" > $logFile
+    if($flags) {
+        # If more flag will be necessary, we have to consider composing the command other way
+        # ie. https://stackoverflow.com/questions/6604089/dynamically-generate-command-line-command-then-invoke-using-powershell
+        podman machine init $flags >> $logFile
+    } else {
+        podman machine init >> $logFile
+    }
+    if ($start -eq "1") {
+        write-host "Starting podman machine..."
+        "podman machine start" >> $logfile
+        podman machine start >> $logFile
+    }
+    podman machine ls >> $logFile
+} else {
+    write-host "Podman installed, no machine prepared..."
+}
 
 write-host "Script finished..."
