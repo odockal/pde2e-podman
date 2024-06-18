ca5dc653012b9057613d08c06c5dda7e1b3a7388
Ondrej Dockal <odockal@redhat.com>
2024-06-18 12:18:38 +0200

    create a tekton task for pde2e-podman image
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- builder.sh
+++ builder.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Build Mac OS image
-echo "### Building and pushing darwin image ###"
 OS=darwin make oci-build
 OS=darwin make oci-push
 
 # Build Windows image
-echo "### Building and pushing windows image ###"
 OS=windows make oci-build
 OS=windows make oci-push
+
+# Build and push Tekton Task image
+make tkn-push
 
 
